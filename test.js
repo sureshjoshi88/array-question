672dc09471fc4e7d8e5f6188de8eabdaf56ae8b3
sureshjoshi88 <sj3795789@gmail.com>
2024-12-01 16:04:42 +0530

    add new array question

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 console.log("suresh");
+
+
+var arr = [12,33,44,55,66,77,645,7,5,6,45,6,23]
+var sum = 0;
+var index = 1;
+for(var i = 0;i<arr.length;i++){
+    if(arr[i]%2!=0){
+        sum+=arr[i];
+        index++;
+    }
+}
+var total= sum/index;
+console.log(total);
