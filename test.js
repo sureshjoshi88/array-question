036cd03065f1f3a713ea5d9cfdc9e791e4553027
sureshjoshi88 <sj3795789@gmail.com>
2024-12-04 21:59:28 +0530

    remove

--- test.js
+++ test.js
@@ -23,8 +23,6 @@ function myfunc (agge){
 let result = myfunc(agge.find(myfunc));
 console.log(result); 
 
-
-
 let arr = [1,2,3,4,5,6]
 let values1 = arr.map((number)=>{
     let final = number*=4;
