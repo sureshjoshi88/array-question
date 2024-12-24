5af3cd4282f8ba6d507d00e898780e5ea17e9fc5
sureshjoshi88 <sj3795789@gmail.com>
2024-12-24 16:18:53 +0530

    add new switch case

--- test.js
+++ test.js
@@ -148,3 +148,32 @@ let mypromise = new Promise((resolve,reject)=>{
 }).catch((value)=>{
     console.log(value+=1000,"this is odd");
 });
+
+
+let day  = 3;
+
+switch(day){
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thirsday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    default:
+        console.log("invalid days");    
+}
