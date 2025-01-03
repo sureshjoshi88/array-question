06e84d8a70481e8970759f95f23a0358b1db88e1
sureshjoshi88 <sj3795789@gmail.com>
2025-01-03 11:22:54 +0530

    edit question

--- test.js
+++ test.js
@@ -31,7 +31,7 @@ let values1 = arr.map((number)=>{
 console.log(values1);
 
 
-const numbers = [1,2,3,4,5,6,7,8];
+const numbers = [1,2,3,4,5,6,7,8,9,10];
 let arr  = numbers.filter((num)=>num%2!=0);
 console.log(arr)
 
